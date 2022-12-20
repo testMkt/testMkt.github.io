bddd6bd95fc97176271837a85ff6c3daafa99b57
demo <121090777+testMkt@users.noreply.github.com>
2022-12-20 21:15:44 +0900

    Update TestGithubApplication.java

--- src/main/java/jp/co/demo/TestGithubApplication.java
+++ src/main/java/jp/co/demo/TestGithubApplication.java
@@ -2,12 +2,19 @@ package jp.co.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class TestGithubApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(TestGithubApplication.class, args);
 	}
 
+	@GetMapping("/3")
+	pulic String test() {
+		return "index";
+	}
 }
